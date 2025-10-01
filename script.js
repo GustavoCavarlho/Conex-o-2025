d38b8ff5007ad21f76ca09e479fa3dda39cf6f15
GustavoCavarlho <carvalho.souza28@escola.pr.gov.br>
2025-10-01 11:42:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ $(".theme-switch").on("click", () => {
     ("light-theme");
 });
 
+$(document).ready(function(){
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){ $('#autoWidth').removeClass('cS-hidden');}
+    });
+});
+
+
+
+
+
+
+
+
+
